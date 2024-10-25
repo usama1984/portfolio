cb5abd9eb55b20352bff2500f4dd0bdd0cf30e90
usama1984 <usamamuhammad231@gmail.com>
2024-10-25 16:50:03 +0400

    experience and skills update

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './Experience.css'
-import { HiBadgeCheck } from 'react-icons/hi'
+import React from "react";
+import "./Experience.css";
+import { HiBadgeCheck } from "react-icons/hi";
 
 const Experience = () => {
   return (
-    <section id='skills'>
+    <section id="skills">
       <h5>What Skills I Have</h5>
       <h2>Skills</h2>
 
@@ -12,59 +12,66 @@ const Experience = () => {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>Angular</h4>
-              <small className='text-light'>4 Years</small>
+                <h4>Angular</h4>
+                <small className="text-light">5+ Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>WordPress</h4>
-              <small className='text-light'>5+ Years</small>
+                <h4>WordPress</h4>
+                <small className="text-light">5+ Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>TypeScript</h4>
-              <small className='text-light'>4 Years</small>
+                <h4>TypeScript</h4>
+                <small className="text-light">4+ Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>JavaScript</h4>
-              <small className='text-light'>4 Years</small>
+                <h4>JavaScript</h4>
+                <small className="text-light">4+ Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>HTML</h4>
-              <small className='text-light'>5+ Years</small>
+                <h4>HTML</h4>
+                <small className="text-light">5+ Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>Tailwind CSS</h4>
-              <small className='text-light'>3+ Years</small>
+                <h4>Tailwind CSS</h4>
+                <small className="text-light">3+ Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>React</h4>
-              <small className='text-light'>6 Months</small>
+                <h4>React</h4>
+                <small className="text-light">6 Months</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
+              <div>
+                <h4>Shopify</h4>
+                <small className="text-light">2+ Years</small>
               </div>
             </article>
           </div>
@@ -73,55 +80,54 @@ const Experience = () => {
         <div className="experience__backend">
           <h3>Backend Development</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>Nest.js</h4>
-              <small className='text-light'>2 Months</small>
+                <h4>Nest.js</h4>
+                <small className="text-light">2 Months</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>Postgres SQL</h4>
-              <small className='text-light'>2 Months</small>
+                <h4>Postgres SQL</h4>
+                <small className="text-light">2 Months</small>
               </div>
             </article>
-
           </div>
 
-          <h3 className='mt-2'>Other Skills</h3>
+          <h3 className="mt-2">Other Skills</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>PhotoShop</h4>
-              <small className='text-light'>2+ Years</small>
+                <h4>PhotoShop</h4>
+                <small className="text-light">3+ Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>Illustrator</h4>
-              <small className='text-light'>2+ Years</small>
+                <h4>Illustrator</h4>
+                <small className="text-light">3+ Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>Figma</h4>
-              <small className='text-light'>2 Years</small>
+                <h4>Figma</h4>
+                <small className="text-light">4 Years</small>
               </div>
             </article>
 
-            <article className='experience__details'>
-              <HiBadgeCheck className='experience__details-icon' />
+            <article className="experience__details">
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>Canva</h4>
-              <small className='text-light'>2 Years</small>
+                <h4>Canva</h4>
+                <small className="text-light">3+ Years</small>
               </div>
             </article>
           </div>
@@ -165,7 +171,7 @@ const Experience = () => {
         </div> */}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
